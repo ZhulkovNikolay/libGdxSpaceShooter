de4c4a46fb60316554ad1141cc0fcd15c10bb7ca
ZhulkovNikolay <wed10@yandex.ru>
2018-08-11 01:19:22 +0300

    firstCommit

---
+++ desktop/src/ru/geekbrains/spacefly/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.spacefly.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.geekbrains.spacefly.SpaceFly;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceFly(), config);
+	}
+}
